631861311b2de293167950789b9dad68158f1ab2
Valentina Filonova <filonova2001v@gmail.com>
2021-12-15 08:13:21 +0300

    sc

--- src/main/java/com/example/demo/FirstController.java
+++ src/main/java/com/example/demo/FirstController.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,23 +13,39 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
-@RestController
+@Controller
 public class FirstController {
-    @RequestMapping("/")
-    public String home() {
-        return "index.html";
+
+    @RequestMapping(value = "/")
+    public String start() {
+//        model.addAttribute("name", name);
+        return "index";
     }
 
-    @RequestMapping(value = "/getDateAndTime")
-    public ModelAndView getDateAndTime() {
-
-        var now = LocalDateTime.now();
-        var dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        var date_time = dtf.format(now);
-
-        var params = new HashMap<String, Object>();
-        params.put("date_time", date_time);
-
-        return new ModelAndView("showMessage", params);
+    @RequestMapping(value = "/greeting")
+    public String helloWorldController(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
+
 }
+//@RestController
+//public class FirstController {
+//    @RequestMapping("/")
+//    public String home() {
+//        return "index.html";
+//    }
+//
+//    @RequestMapping(value = "/getDateAndTime")
+//    public ModelAndView getDateAndTime() {
+//
+//        var now = LocalDateTime.now();
+//        var dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+//        var date_time = dtf.format(now);
+//
+//        var params = new HashMap<String, Object>();
+//        params.put("date_time", date_time);
+//
+//        return new ModelAndView("showMessage", params);
+//    }
+//}
